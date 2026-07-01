8cd6455d2a18424815d27d2efe90ce81e6178308
24BCAA-Anwitha <anwithavpb@gmail.com>
2026-07-01 09:50:51 +0530

    Refactor isPrime function with documentation and test update
    Updated the isPrime function to include JSDoc comments and changed the test number from 17 to 15.

--- prime.js
+++ prime.js
@@ -1,18 +1,23 @@
+/**
+ * Checks if a number is a prime number.
+ * @param {number} num - The number to check.
+ * @returns {boolean} True if prime, false otherwise.
+ */
 const isPrime = (num) => {
-    if(num<=1){
+    if(num <= 1){
         return false;
     }
-    for (let i = 2;i<num;i++){
-        if (num%i===0){
-            return false;
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){
+            return false
         }
     }
     return true;
 }
-let number = 17;
+
+let number = 15;
 if(isPrime(number)){
-    console.log(number = "is a Prime Number");
+    console.log(number + "is a prime number");
 }else{
-    console.log(number = "is not a Prime Number");
+    console.log(number + "is not a prime number");
 }
-isPrime(17);
